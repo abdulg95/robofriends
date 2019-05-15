c42014763c670e0ec50947f30ef44f9d0f595d3f
abdulg95 <abdulg1995@gmail.com>
2019-05-15 03:02:46 -0400

    changed project to use redux

--- src/index.js
+++ src/index.js
@@ -1,9 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
+import {Provider} from 'react-redux';
+import {createStore,applyMiddleware,combineReducers} from 'redux';
 import App from './containers/App';
+import {createLogger} from 'redux-logger';
 import registerServiceWorker from './registerServiceWorker';
 import 'tachyons';
+import {searchRobots,requestRobots} from './reducers';
+import thunkMiddleware from 'redux-thunk';
+const logger = createLogger();
+const rootReducer = combineReducers({searchRobots,requestRobots})
+const store = createStore(rootReducer,applyMiddleware(thunkMiddleware,logger));
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(<Provider store ={store}>
+                    <App/>
+                </Provider>, document.getElementById('root'));
 registerServiceWorker();
